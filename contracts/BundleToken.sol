7bf04f2871619c5dcd1a08a6db2f66825566b877
sharad-s <sharadshekar@gmail.com>
2018-06-07 20:31:43 -0500

    deletions

--- contracts/BundleToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.17;
-
-import "zeppelin-solidity/contracts/token/MintableToken.sol";
-
-contract TheSimplestToken is MintableToken {
-    string public constant name = " TheSimplestToken ";
-    string public constant symbol = "TST";
-    uint32 public constant decimals = 18;
-}
